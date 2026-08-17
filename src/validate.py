6aec079df3ca8ff747e3c0fb747ce31e338d06f8
Thiago <thiagoferreiralopes262007@gmail.com>
2026-08-17 11:57:00 -0300

    Adcionando comentários para explicar cada função

--- src/validate.py
+++ src/validate.py
@@ -1,17 +1,33 @@
+#Definindo a função "validate"
 def validate(df):
+
+#============================#
+#Lista para acumular erros encontrados no dataframe
     erros = []
 
+#============================#
+#Verifica se o DataFrame está vazio:
     if df.empty is True:
        erros.append('DataFrame vazio')
 
+#============================#
+#Verifica se tem algum id nulo no dataframe:
     if df['id'].isna().any() != 0:
         erros.append('Existe item nulo em: "id"')
 
+#============================#
+#Verifica se tem algum id com duplicidade:
     if df['id'].duplicated().any() != 0:
         erros.append('Existe duplicidade em: "id"')
 
+#============================#
+#Verifica se as avaliações estão na faixa correta:
     if not df['rating'].dropna().between(0,100).all():
         erros.append('Existe avaliações incorretas em: "rating"')
+
+#============================#
+#Criando uma lista para utilizá-la no FOR para analisar se existe 
+#algum registro por coluna com dados negativos
 
     colunas_negativas = [
         'review_count',
@@ -21,8 +37,12 @@ def validate(df):
         'completionist_hours'
     ]
 
-    for coluna in  colunas_negativas:
+    for coluna in colunas_negativas:
         if (df[coluna].dropna() < 0).any():
             erros.append(f'Existe números negativos em: {coluna}')
-            
+
+#============================#
+#Retorna a lista com todos os erros do dataframe ;)
     return erros
+
+#============================#
